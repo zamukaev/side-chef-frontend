fce74ace638e31119a8ce8b5b781af601eb60480
zamukaev <abu.safiia2016@mail.com>
2023-06-15 11:34:37 +0200

    feature/SC-22_add css variables,i18n,react-icons

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import { createRoot } from 'react-dom/client';
 import App from './app/App';
 
+import { Suspense } from 'react';
+
 const root = createRoot(document.getElementById('root'));
+import "./shared/config/i18n";
 
 root.render(
-    <App />
+    <Suspense fallback="loading">
+        <App />
+    </Suspense>
 );
