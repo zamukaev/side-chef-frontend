4127ed624d56b99da651ddd415d6b0886633e297
zamukaev <abu.safiia2016@mail.com>
2023-06-18 13:45:12 +0200

    fix lint

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,13 +2,13 @@ import { useTranslation } from "react-i18next";
 import "./styles/index.scss";
 
 const App = () => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    return (
-        <div className={"app" + " light"}>
-            <h1>{t("hello world")}</h1>
-        </div>
-    );
+  return (
+    <div className={"app" + " light"}>
+      <h1>{t("hello world")}</h1>
+    </div>
+  );
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import { createRoot } from 'react-dom/client';
-import { Suspense } from 'react';
-import App from './app/App';
+import { createRoot } from "react-dom/client";
+import { Suspense } from "react";
+import App from "./app/App";
 
-import './shared/config/i18n';
+import "./shared/config/i18n";
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 
 root.render(
   <Suspense fallback="loading">
