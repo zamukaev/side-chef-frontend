4df559965cb0b9e3e14e9239e1113cc75d423d26
zamukaev <abu.safiia2016@mail.com>
2023-07-13 23:00:21 +0200

    resove conflicts

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,5 +1,6 @@
 import { Logo } from "./logo/Logo";
 import { BurgerMenu } from "./burgerMenu/BurgerMenu";
 import { LogoDrawer } from "./logoDrawer/LogoDrawer";
+import { Button, ThemeButton } from "./button/Button";
 
-export { Logo, LogoDrawer, BurgerMenu };
+export { Logo, LogoDrawer, BurgerMenu, Button, ThemeButton };
